7c13601a50bcad65c76efc57c0803fa2681160a3
Uthman Durosinlohun <udurosinlohun@gmail.com>
2023-06-26 23:49:07 +0000

    fix bug in task 7

--- 0x00-ES6_basic/7-getBudgetObject.js
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -1,8 +1,8 @@
 export default function getBudgetObject(income, gdp, capita) {
   const budget = {
-    income: parseInt(`${income}`, 10),
-    gdp: parseInt(`${gdp}`, 10),
-    capita: parseInt(`${capita}`, 10),
+    income: parseInt(income, 10),
+    gdp: parseInt(gdp, 10),
+    capita: parseInt(capita, 10),
   };
 
   return budget;
