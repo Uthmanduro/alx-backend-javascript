4bfc331e42496988eefac4c4f282a2955bd779e3
Uthman Durosinlohun <udurosinlohun@gmail.com>
2023-06-26 21:29:46 +0000

    added js files

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,9 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const newarray = [];
+
+  for (const value of array) {
+    newarray.push(appendString + value);
+  }
+
+  return newarray;
+}
