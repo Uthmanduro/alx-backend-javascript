920c7644aa24097560b5db015628133c0746a678
Uthman Durosinlohun <udurosinlohun@gmail.com>
2023-06-28 02:04:40 +0000

    solved till task 5

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,11 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  uploadPhoto().then((result) => {
+    createUser().then((res) => {
+      const firstname = res.firstName;
+      const lastname = res.lastName;
+      console.log(result.body, firstname, lastname);
+    });
+  });
+}
