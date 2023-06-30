601abec082b0eb2d3afcee2243f161c690eb03d4
Uthman Durosinlohun <udurosinlohun@gmail.com>
2023-06-30 01:21:04 +0000

    task 1-4

---
+++ 0x02-ES6_classes/setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# cp ../packages/babel.config.js ../packages/package.json ../packages/.eslintrc.js ./
+npm install --save-dev jest
+npm install --save-dev babel-jest @babel/core @babel/preset-env
+npm install --save-dev eslint
