a28ba202acbe411525fb4a6d7dd506f0d3c54211
Uthman Durosinlohun <udurosinlohun@gmail.com>
2023-07-01 01:11:46 +0000

    task 5-9

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,33 @@
+export default class Airport {
+  /* eslint-disable no-underscore-dangle */
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = value;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Code must be a string');
+    }
+    this._code = value;
+  }
+
+  toString() {
+    return this._code;
+  }
+}
