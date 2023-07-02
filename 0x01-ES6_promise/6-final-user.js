730d3025932f09db6eb0850f090ac3fc78fdebf5
Uthman Durosinlohun <udurosinlohun@gmail.com>
2023-07-02 12:32:15 +0000

    fixed bug promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,5 +4,18 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const user = signUpUser(firstName, lastName);
   const photo = uploadPhoto(fileName);
-  return Promise.allSettled([user, photo]);
+  return Promise.allSettled([user, photo])
+    .then((results) => {
+      const arr = [];
+      for (const result of results) {
+        if (result.status === 'rejected') {
+          const value = result.reason.message;
+          arr.push({ status: result.status, value });
+        } else {
+          const { value } = result;
+          arr.push({ status: result.status, value });
+        }
+      }
+      return arr;
+    });
 }
