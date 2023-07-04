8e05eeee328373919f5c809274c707d95f0de111
Uthman Durosinlohun <udurosinlohun@gmail.com>
2023-07-04 01:46:55 +0000

    fixed bug in task 10

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,15 +1,10 @@
 export default function updateUniqueItems(newmap) {
+  if (!(newmap instanceof Map)) {
+    throw new TypeError('Cannot process');
+  }
   newmap.forEach((val, key) => {
-    try {
-      if (newmap.get(key) === 1) {
-        newmap.set(key, 100);
-      }
-    } catch (error) {
-      if (error.name === 'TypeError') {
-        throw new TypeError('Cannot process');
-      } else {
-        console.error(error);
-      }
+    if (newmap.get(key) === 1) {
+      newmap.set(key, 100);
     }
   });
 }
