9fc87793398dbffb530f1ccf3298fd309bc73a90
Uthman Durosinlohun <udurosinlohun@gmail.com>
2023-07-05 05:38:41 +0000

    student by city id

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,4 +1,14 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
-  studentbycity = students.filter(arr => arr.location === city);
-
+  /* eslint-disable no-param-reassign */
+  const studentbycity = students.filter((newarr) => newarr.location === city);
+  studentbycity.map((obj) => {
+    const studentgrade = newGrades.filter((arr) => arr.studentId === obj.id);
+    if (studentgrade.length === 0) {
+      obj.grade = 'N/A';
+    } else {
+      obj.grade = studentgrade[0].grade;
+    }
+    return obj;
+  });
+  return studentbycity;
 }
