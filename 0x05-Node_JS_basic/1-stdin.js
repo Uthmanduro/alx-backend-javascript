c2733db22c1fd8702f70faf79511da3bcebe1b16
uthmanduro <udurosinlohun@gmail.com>
2023-08-22 22:53:40 +0100

    task 1 and 2

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,12 @@
+const { stdout, stdin } = process;
+
+stdout.write('Welcome to Holberton School, what is your name?\n');
+
+stdin.on('readable', () => {
+  stdout.write(`Your name is: ${stdin.read()}`);
+  process.exit();
+});
+
+process.on('exit', () => {
+  stdout.write('This important software is now closing\n');
+});
