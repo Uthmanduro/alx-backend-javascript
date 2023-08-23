4683ea5fbeefa829e18b863ac1bd3f51558a7ebe
uthmanduro <udurosinlohun@gmail.com>
2023-08-23 18:47:08 +0100

    trimmed data from file

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -4,28 +4,28 @@
 const fs = require('fs');
 
 function countStudents(filePath) {
+  let data;
   try {
-    const data = fs.readFileSync(filePath, 'utf8');
-    const splitData = data.split('\r\n');
-    splitData.shift();
-    const studentData = {};
-    for (let student of splitData) {
-      student = student.split(',');
-      if (student[3] in studentData) {
-        studentData[student[3]].push(student[0]);
-      } else {
-        studentData[student[3]] = [];
-        studentData[student[3]].push(student[0]);
-      }
-    }
-    console.log(`Number of students: ${splitData.length}`);
-    for (const fields in studentData) {
-      if (fields) {
-        console.log(`Number of students in ${fields}: ${studentData[fields].length}. List: ${studentData[fields].join(', ')}`);
-      }
-    }
+    data = fs.readFileSync(filePath, 'utf8');
   } catch (err) {
     throw new Error('Cannot load the database');
+  }
+  data.trim();
+  const splitData = data.split('\r\n');
+  splitData.shift();
+  const studentData = {};
+  for (let student of splitData) {
+    student = student.split(',');
+    if (student[3] in studentData) {
+      studentData[student[3]].push(student[0]);
+    } else {
+      studentData[student[3]] = [];
+      studentData[student[3]].push(student[0]);
+    }
+  }
+  console.log(`Number of students: ${splitData.length}`);
+  for (const fields in studentData) {
+    console.log(`Number of students in ${fields}: ${studentData[fields].length}. List: ${studentData[fields].join(', ')}`);
   }
 }
 
