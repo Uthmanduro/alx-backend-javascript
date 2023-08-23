117984c072d7f00b948622a609b46c02d88448cb
uthmanduro <udurosinlohun@gmail.com>
2023-08-23 18:08:23 +0100

    refactored code

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -9,26 +9,23 @@ function countStudents(filePath) {
       throw new Error('Cannot load the database');
     }
     const splitData = data.split('\r\n');
-    const studentData = {
-      cs_count: 0,
-      swe_count: 0,
-      cs_students: [],
-      swe_students: [],
-    };
-    const { length } = splitData;
-    for (let rows = 1; rows < length; rows += 1) {
-      const student = splitData[rows].split(',');
-      if (student[3] === 'CS') {
-        studentData.cs_count += 1;
-        studentData.cs_students.push(student[0]);
-      } else if (student[3] === 'SWE') {
-        studentData.swe_count += 1;
-        studentData.swe_students.push(student[0]);
+    splitData.shift();
+    const studentData = {};
+    for (let student of splitData) {
+      student = student.split(',');
+      if (student[3] in studentData) {
+        studentData[student[3]].push(student[0]);
+      } else {
+        studentData[student[3]] = [];
+        studentData[student[3]].push(student[0]);
       }
     }
-    console.log(`Number of students: ${splitData.length - 1}`);
-    console.log(`Number of students in CS: ${studentData.cs_count}. List: ${studentData.cs_students.join(', ')}`);
-    console.log(`Number of students in SWE: ${studentData.swe_count}. List: ${studentData.swe_students.join(', ')}`);
+    console.log(`Number of students: ${splitData.length}`);
+    for (const fields in studentData) {
+      if (fields) {
+        console.log(`Number of students in ${fields}: ${studentData[fields].length}. List: ${studentData[fields].join(', ')}`);
+      }
+    }
   });
 }
 
