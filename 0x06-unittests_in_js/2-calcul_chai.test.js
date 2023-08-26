6d4aced2c609138d663762336f155b6b503a95fc
uthmanduro <udurosinlohun@gmail.com>
2023-08-26 04:03:43 +0100

    fix err in 2

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,5 +1,5 @@
 const { expect } = require('chai');
-const calculateNumber = require('./2-calcul.js');
+const calculateNumber = require('./2-calcul_chai');
 
 describe('calculateNumber', () => {
   it('checks the output based on type', () => {
