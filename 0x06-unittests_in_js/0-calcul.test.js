6932138ba5bbf47f00419f7a97fbaaf6613ec451
uthmanduro <udurosinlohun@gmail.com>
2023-08-26 03:59:34 +0100

    fixed errors

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -8,4 +8,7 @@ describe('calculateNumber', () => {
     assert.strictEqual(calculateNumber(1.2, 3.7), 5);
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
   });
+  it('Should return 5', function () {
+    assert.equal(calculateNumber(1, 3.7), 5);
+  });
 });
